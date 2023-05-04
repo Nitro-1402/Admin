4998ca6d846aa5a4a737376b60a2e86cb13ba204
AliKontratchi <alikontratchi200.com>
2023-05-04 17:40:43 +0430

    feat/admin-movie

--- client/src/components/Post/PostCreate.js
+++ client/src/components/Post/PostCreate.js
@@ -1,25 +1,66 @@
-import React from 'react'
-import { Create, SimpleForm, TextInput, DateInput } from 'react-admin'
-import { ImageInput, ImageField } from 'react-admin';
-
+import React from "react";
+import {
+  Create,
+  SimpleForm,
+  TextInput,
+  DateInput,
+  SelectInput,
+} from "react-admin";
+import { ImageInput, ImageField } from "react-admin";
+import { SelectArrayInput } from "react-admin";
+import { FileInput, FileField } from "react-admin";
 const PostCreate = (props) => {
   return (
-    <Create title='ایجاد پست' {...props}>
+    <Create title="ایجاد پست" {...props}>
       <SimpleForm>
-        <TextInput source='title' label="عنوان"/>
-        <TextInput multiline source='description' label="توضیحات" />
-        <DateInput label='تاریخ انتشار' source='publish_date' />
-        <ImageInput source="poster" label="عکس">
-            <ImageField source="photo" title="title" />
+        <TextInput source="title" label="عنوان" />
+        {/* <SelectInput source='actors' label="بازیگران"/> */}
+
+        <SelectArrayInput
+          source="actors"
+          label="بازیگران"
+          choices={[
+            { id: 1, name: "Admin" },
+            { id: 2, name: "Editor" },
+            { id: 3, name: "Moderator" },
+            { id: 4, name: "Reviewer" },
+          ]}
+        />
+        <SelectArrayInput
+          source="category_set"
+          label="ژانر"
+          choices={[
+            { id: 1, name: "Admin" },
+            { id: 2, name: "Editor" },
+            { id: 3, name: "Moderator" },
+            { id: 4, name: "Reviewer" },
+          ]}
+        />
+
+        <SelectInput
+          source="director"
+          label="کاگردان"
+          choices={[
+            { id: 1, name: "Tech" },
+            { id: 2, name: "Lifestyle" },
+            { id: 3, name: "People" },
+          ]}
+        />
+
+        <ImageInput source="poster" accept="image/*">
+        <ImageField source="src" title="title" />
         </ImageInput>
+
+        <FileInput source="thumbnail">
+            <FileField source="thumbnail" title="نمیدانم" />
+        </FileInput>
+
+        <TextInput multiline source="description" label="توضیحات" />
+
+        <DateInput label="تاریخ انتشار" source="publish_date" />
       </SimpleForm>
     </Create>
-  )
-}
+  );
+};
 
-export default PostCreate
-
-
-
-
-
+export default PostCreate;
